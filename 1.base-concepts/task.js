2cbb0f8a47e4632302390666bb0b19326a87c6bc
1000karat <92o3o92562@gmail.com>
2022-09-11 19:24:10 +0300

    add code for 1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,28 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr;
+
+    let d = b ** 2 - 4 * a * c;
+
+    if (d < 0) {
+        return (arr = []);
+    }
+    if (d === 0) {
+        let x = -b / (2 * a);
+        return (arr = [x]);
+    }
+    if (d > 0) {
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return (arr = [x1, x2]);
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
