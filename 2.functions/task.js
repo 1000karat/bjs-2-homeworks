ac8e90146771af22863290de636c8f1ceedf038b
1000karat <92o3o92562@gmail.com>
2022-09-28 15:20:14 +0300

    add code for 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    let avg = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum += arr[i];
+    }
+    avg = Number((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+    return {min: min, max: max, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+    let max = -Infinity;
+    for (let i = 0; i < arrOfArr.length; i++) {
+        let arrSum = func(arrOfArr[i]);
+        if (arrSum > max) {
+            max = arrSum;
+        }
+    }
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let arrParams = getArrayParams(arr);
+
+    return arrParams.max - arrParams.min;
 }
